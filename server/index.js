b36ee9de0de237d3fc732902b74286dcfe4548d8
Kapil Solanke <kapilsolanke7263@gmail.com>
2023-07-11 19:11:20 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"*",
 		credentials:true,
 	})
 )
@@ -31,7 +31,7 @@ app.use(
 app.use(
 	fileUpload({
 		useTempFiles:true,
-		tempFileDir:"/tmp",
+		tempFileDir:"/tmp/",
 	})
 )
 //cloudinary connection
